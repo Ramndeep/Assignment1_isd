98ef34609b057859be6d26ab97013a0b62881d7d
Raman <Ramandeepkaur16@acedemic.com>
2024-09-15 20:10:13 -0600

    added more tests to meet all the requirements

--- tests/test_client.py
+++ tests/test_client.py
@@ -33,3 +33,23 @@ class TestClient(unittest.TestCase):
     def test_invalid_email(self):
         client = Client(1010, "Susan", "Clark", "invalid-email")
         self.assertEqual(client.email_address, "email@pixell-river.com")
+    
+    def test_client_number_getter(self):
+        client = Client(1010, "Susan", "Clark", "susanclark@pixell.com")
+        self.assertEqual(client.client_number, 1010)
+
+    def test_first_name_getter(self):
+        client = Client(1010, "Susan", "Clark", "susanclark@pixell.com")
+        self.assertEqual(client.first_name, "Susan")
+
+    def test_last_name_getter(self):
+        client = Client(1010, "Susan", "Clark", "susanclark@pixell.com")
+        self.assertEqual(client.last_name, "Clark")
+
+    def test_email_address_getter(self):
+        client = Client(1010, "Susan", "Clark", "susanclark@pixell.com")
+        self.assertEqual(client.email_address, "susanclark@pixell.com")
+
+    def test_str_method(self):
+        client = Client(1010, "Susan", "Clark", "susanclark@pixell.com")
+        self.assertEqual(str(client), "Clark, Susan [1010] - susanclark@pixell.com\n")
